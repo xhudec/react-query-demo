5d2472d10e7cd4c413b93b53a47e07620a99710e
Lukas Hudec <hudec.luke@gmail.com>
2021-05-04 13:26:31 +0200

    chore: config adjusments

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,6 @@ module.exports = {
   extends: ['@strv/stylelint-config-styled-components', 'stylelint-config-prettier'],
   rules: {
     'declaration-empty-line-before': null,
-    'unit-allowed-list': ['ms', 's', 'deg', '%', 'rem', 'vh', 'vw', 'fr', 'ch'],
+    'unit-allowed-list': ['ms', 's', 'deg', '%', 'rem', 'vh', 'vw', 'fr', 'ch', 'px'],
   },
 }
